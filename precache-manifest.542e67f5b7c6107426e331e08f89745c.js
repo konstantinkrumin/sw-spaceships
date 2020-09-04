cf165d8b1265d2dfb37aed4752c1428124d8b6b8
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-04 17:43:00 +0300

    Updates

---
+++ precache-manifest.542e67f5b7c6107426e331e08f89745c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b259d931a3ff2b9c68488445c2966f",
+    "url": "/sw-spaceships/index.html"
+  },
+  {
+    "revision": "413229a60a54d6b91d84",
+    "url": "/sw-spaceships/static/css/main.e59514f1.chunk.css"
+  },
+  {
+    "revision": "a8518d359fbf3d0a8081",
+    "url": "/sw-spaceships/static/js/2.be8deb94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sw-spaceships/static/js/2.be8deb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413229a60a54d6b91d84",
+    "url": "/sw-spaceships/static/js/main.4b99fec3.chunk.js"
+  },
+  {
+    "revision": "2f1553c41c9ef6f25ca6",
+    "url": "/sw-spaceships/static/js/runtime-main.b4aa8f60.js"
+  },
+  {
+    "revision": "91681c57cd94b429fd9a5bdcb92c2f9c",
+    "url": "/sw-spaceships/static/media/space.91681c57.jpg"
+  }
+]);
