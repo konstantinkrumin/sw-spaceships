6ba25392c2be7cd211419d9aa9ce13a6be9ad76f
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-04 20:58:19 +0300

    changing replace function in fetching hook

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -7,12 +7,28 @@ export default function useFetch(url) {
   async function fetchUrl() {
     let results = [];
 
-    do {
+    // do {
+    //   const res = await fetch(url);
+    //   const data = await res.json();
+    //   // if (data !== null) {
+    //   //   url = data.next.replace(/^http:\/\//i, 'https://');
+    //   // }
+    //   url = data.next;
+    //   console.log(url);
+    //   results.push(...data.results);
+    // } while (url);
+
+    while (url !== null) {
       const res = await fetch(url);
       const data = await res.json();
-      url = data.next;
+      if (data.next !== null) {
+        data.next = data.next.replace(/^http:\/\//i, 'https://');
+      }
       results.push(...data.results);
-    } while (url);
+      url = data.next;
+      console.log(data);
+      // results.push(...data.results);
+    }
 
     setData(results);
     setLoading(false);
